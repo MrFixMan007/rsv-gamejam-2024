ce28390425075f2ccdefef8ec0238cf484cfbb35
vlsbd <91143641+vlsbd@users.noreply.github.com>
2024-06-29 00:06:56 +0300

    Time bar

---
+++ game/Game/Assets/Scripts/UI/TimeBar.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeBar : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    public float max_time;
+    float time_left;
+    void Start()
+    {
+        time_left = max_time;
+    }
+    void Reset()
+    {
+        time_left = max_time;
+    }
+    void Update()
+    {
+        Debug.Log(time_left);
+        if (time_left > 0) 
+        {
+            time_left -= Time.deltaTime;
+            slider.value = time_left / max_time;
+        }
+    }
+}
