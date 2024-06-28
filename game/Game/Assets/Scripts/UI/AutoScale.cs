971779ca6c42d6c574b4a70aa541300d4f24b817
vlsbd <91143641+vlsbd@users.noreply.github.com>
2024-06-28 21:12:54 +0300

    UI, автоскейл

---
+++ game/Game/Assets/Scripts/UI/AutoScale.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoScale : MonoBehaviour
+{
+    private Camera main_cam;
+    void Start()
+    {
+        main_cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        transform.position = new Vector3(main_cam.transform.position.x, main_cam.transform.position.y, 20);
+        Vector3 bottom_left = main_cam.ViewportToWorldPoint(Vector3.zero);
+        Vector3 top_right = main_cam.ViewportToWorldPoint(new Vector3(main_cam.rect.width, main_cam.rect.height));
+        Vector3 screen_size = top_right - bottom_left;
+        float current_ratio = screen_size.x / screen_size.y;
+        float optimal_ratio = transform.localScale.x / transform.localScale.y;
+
+        if (current_ratio > optimal_ratio) 
+        {
+            float height = screen_size.y;
+            transform.localScale = new Vector3(height * optimal_ratio, height);
+        }
+        else
+        {
+            float width = screen_size.x;
+            transform.localScale = new Vector3(width, width / optimal_ratio);
+        }
+    }
+}
