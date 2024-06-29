5b6a49e62bb9f4302a776ae4d2621d7b2ff9f8ac
vlsbd <91143641+vlsbd@users.noreply.github.com>
2024-06-29 22:17:56 +0300

    Папки, начало готовки, фикс префабов

---
+++ game/Game/Assets/Scripts/Items/Cooking.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Cooking : Item
+{
+    public TimeBar time_bar;
+    public override void Use()
+    {
+
+    }
+    override protected void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
--- game/Game/Assets/Scripts/UI/Set_camera.cs
+++ game/Game/Assets/Scripts/UI/Set_camera.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Set_camera : MonoBehaviour
